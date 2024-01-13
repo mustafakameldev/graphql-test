47d83e5325609bc8934082e3d91054735b2ec291
Mustafa Kamel <37935806+mustafakameldev@users.noreply.github.com>
2024-01-13 15:51:25 +0300

    update/delete student  and test validation

--- src/resolvers.js
+++ src/resolvers.js
@@ -32,12 +32,42 @@ const Mutation = {
   },
   addStudent: (root, args, context, info) => {
     const id = db.students.create({
-      collegeId: args.collegeId,
-      firstName: args.firstName,
-      lastName: args.lastName,
+      collegeId: !args.collegeId,
+      firstName: !args.firstName,
+      lastName: !args.lastName,
     });
 
     return db.students.get(id);
+  },
+  signUp: (root, args, context, info) => {
+    // testing validation
+    const { email, firstName, password } = args.input;
+
+    const emailExpression =
+      /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+    const isValidEmail = emailExpression.test(String(email).toLowerCase());
+    if (!isValidEmail) throw new Error('email not in proper format');
+
+    if (firstName.length > 15)
+      throw new Error('firstName should be less than 15 characters');
+
+    if (password.length < 8)
+      throw new Error('password should be minimum 8 characters');
+
+    return 'success';
+  },
+  deleteStudent: (root, args, context, info) => {
+    return db.students.delete(args.id);
+  },
+  updateStudent: (root, args, context, info) => {
+    db.students.update({
+      id: args.id,
+      collegeId: args.collegeId ?? '',
+      firstName: args.firstName ?? '',
+      lastName: args.lastName ?? '',
+    });
+    return db.students.get(args.id);
   },
 };
 
